cf931d53508ac31393fc37e925c2816bb93e7893
LeDrumond <leandro.drumond@gmail.com>
2021-01-21 08:35:14 -0300

    Principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [ 
+  { 
+    
+    path: 'config', 
+    loadChildren: () => import('./config/config.module').then(m => m.ConfigModule) 
+  
+  },
+
+  { 
+    
+    path: '', 
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-po-project';
+
+  readonly menus: Array<PoMenuItem> = [
+    //{ label: 'Home', action: this.onClick.bind(this) }
+    { label: 'Início', link: '/' }, { label: 'Config', link: 'config' }
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
+  ngOnInit() {
+    this.restore();
+  }
+
+  restore() {
+      //alert('Só Jesus salva!!!');
+  }
+
 }
